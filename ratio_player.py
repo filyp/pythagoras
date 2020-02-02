799acde817d3004a2a98a32aba01fdb313aae9b6
fsondej <filipsondej@protonmail.com>
2020-02-02 22:10:42 +0100

    initial commit

---
+++ ratio_player.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+import csv
+import curses
+import datetime
+import math
+import os
+import pathlib
+import sys
+import threading
+from time import sleep, time
+
+import numpy as np
+import pyaudio
+from colorama import Fore, Style, init
+from pyfiglet import figlet_format
+from readchar import readchar
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+FILENAME = os.path.join(dir_path, 'ratios.csv')
+BIT_RATE = 44000
+
+# credit: stackoverflow.com/questions/10702942/note-synthesis-harmonics-violin-piano-guitar-bass-frequencies-midi
+violin_amps = [0.0, 1.0, 0.286699025, 0.150079537, 0.042909002,
+               0.203797365, 0.229228698, 0.156931925,
+               0.115470898, 0.0, 0.097401803, 0.087653465,
+               0.052331036, 0.052922462, 0.038850593,]
+               # 0.053554676, 0.053697434, 0.022270261,
+               # 0.013072562, 0.008585879, 0.005771505,
+               # 0.004343925, 0.002141371, 0.005343231,
+               # 0.000530244, 0.004711017, 0.009014153]
+
+piano_amps = [0.0, 1.0, 0.399064778, 0.229404484, 0.151836061,
+              0.196754229, 0.093742264, 0.060871957,
+              0.138605419, 0.010535002, 0.071021868,]
+              # 0.029954614, 0.051299684, 0.055948288,
+              # 0.066208224, 0.010067391, 0.00753679,
+              # 0.008196947, 0.012955577, 0.007316738,]
+              # 0.006216476, 0.005116215, 0.006243983,
+              # 0.002860679, 0.002558108, 0.0, 0.001650392]
+
+sine_amps = [0.0, 1.0]
+
+
+class PolyphonicPlayer(threading.Thread):
+    segment_duration = 0.01     # in seconds
+    amps = sine_amps          # tone (amplitude of the overtones)
+
+    def __init__(self, base_freq=10, max_voices=10):
+        threading.Thread.__init__(self)
+
+         # initialize pyaudio
+        self.p = pyaudio.PyAudio()
+
+        # for paFloat32 sample values must be in range [-1.0, 1.0]
+        self.stream = self.p.open(format=pyaudio.paFloat32,
+                                channels=1,
+                                rate=BIT_RATE,
+                                output=True)
+        self.alive = True
+        self.base_freq = base_freq
+        self.max_voices = max_voices
+        self.ratios = [0] * max_voices
+        self.phases = [0] * max_voices
+        self.last_time = time()
+
+    def run(self):
+        while self.alive:
+            if not self.ratios:
+                # no frequencies given so be silent
+                sleep(self.segment_duration)
+                continue
+
+            # construct a sound
+            new_time = time()
+            final_sound = 0
+            for i, ratio in enumerate(self.ratios):
+                frequency = ratio * self.base_freq
+                final_sound += self.get_wave(frequency, self.phases[i])
+                self.phases[i] += self.segment_duration * frequency * 2*np.pi
+                self.phases[i] %= 2*np.pi
+
+            final_sound /= self.max_voices    # adjust volume
+
+            self.last_time = new_time
+            self.stream.write(final_sound
+                              .astype(np.float32)
+                              .tobytes())
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+    def get_wave(self, primary_frequency, phase):
+        acc = 0
+        for index, amplitude in enumerate(self.amps):
+            frequency = primary_frequency * index
+            wave = np.sin(2 * np.pi *
+                          np.arange(BIT_RATE * self.segment_duration) *
+                          frequency / BIT_RATE
+                          + phase * index)
+            acc += wave * amplitude
+        # adjust volume
+        maximum_possible_volume = np.sum(self.amps)
+        return acc / maximum_possible_volume
+
+    def kill(self):
+        self.alive = False
+
+
+# def set_ratios(self, *ratios):
+#     self.ratios = list(ratios)
+
+
+def get_digit(char):
+    keymap = {
+        'a': '1',
+        's': '2',
+        'd': '3',
+        'f': '4',
+        'v': '5',
+        'n': '6',
+        'j': '7',
+        'k': '8',
+        'l': '9',
+        ';': '0',
+    }
+    if char == 'q':
+        raise KeyboardInterrupt
+    if char in keymap:
+        char = keymap[char]
+    digit = int(char)   # may throw ValueError
+    return digit
+
+
+def color_number(n):
+    color_map = {
+        2: Fore.RED,
+        3: Fore.GREEN,
+        4: Fore.RED,
+        5: Fore.BLUE,
+        6: Fore.YELLOW,
+        8: Fore.RED,
+        9: Fore.GREEN,
+        10: Fore.MAGENTA,
+        12: Fore.YELLOW,
+        15: Fore.CYAN
+    }
+    color = ''
+    if n in color_map:
+        color += color_map[n]
+    return color + str(n) + Style.RESET_ALL
+
+
+def decompose(n, length=6):
+    if n == 0:
+        return ''
+    i = math.floor(math.sqrt(n))
+    while True:
+        if n % i == 0:
+            j = n // i
+            # pan is needed because colored text isn't formatted correctly
+            real_len = len(f'{i}*{j}')
+            pan = ' ' * (6 - real_len)
+            return f'{pan}{color_number(i)}*{color_number(j)}'
+        i -= 1
+
+
+def control(player, verbose=True):
+    template = '\n{1:>6}{2:>6}{3:>6}{4:>6}{5:>6}   {6:>6}{7:>6}{8:>6}{9:>6}{0:>6}    '
+    with open(FILENAME, 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        while True:
+            command = readchar()
+            if command == 'r':
+                writer.writerow([time(), 'node'] + player.ratios)
+                print('node saved', end='  ', flush=True)
+                continue
+            elif command == 'e':
+                writer.writerow([time(), 'edge'] + player.ratios)
+                print('edge saved', end='  ', flush=True)
+                continue
+
+            try:
+                index = get_digit(command)
+                # draw index indicator
+                placeholder = [''] * 10
+                placeholder[index] = '||'
+                print(template.format(*placeholder), end='', flush=True)
+                # read frequency value
+                digit1 = get_digit(readchar())
+                digit2 = get_digit(readchar())
+            except ValueError:
+                print('wrong key', end=' ', flush=True)
+                continue
+            except KeyboardInterrupt:
+                return
+
+            new_ratio = digit1 * 10 + digit2
+            player.ratios[index] = new_ratio
+            print(template.format(*player.ratios), end='', flush=True)
+            if verbose:
+                factors = (decompose(num) for num in player.ratios)
+                print(template.format(*factors), end='', flush=True)
+
+            writer.writerow([time(), 'auto'] + player.ratios)
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) > 1:
+    #     base_freq = float(sys.argv[1])
+    # else:
+    #     base_freq = 5
+    init()
+    player = PolyphonicPlayer()
+    player.start()
+    print(figlet_format('pythagoras', font='graffiti'))
+    control(player)
+    player.kill()
+    player.join()
